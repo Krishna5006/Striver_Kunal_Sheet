92229c96bbf817a7cff661c0379c014760a3b920
Krishna Bisht <hatakekrishna2004@gmail.com>
2026-06-17 02:06:58 +0530

    Kunal Assignment Workspace

--- Kunal_Assignment/src/conditionals_loop/AreaOfCircle.java
+++ Kunal_Assignment/src/conditionals_loop/AreaOfCircle.java
@@ -1,4 +1,12 @@
 package conditionals_loop;
 
+import java.util.Scanner;
+
 public class AreaOfCircle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int radius=sc.nextInt();
+        double area=Math.PI*radius*radius;
+        System.out.println("Area of Circle is :" +area);
+    }
 }
